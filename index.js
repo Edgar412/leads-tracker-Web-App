c1493b4f2a7cab9a41947dfa3ac525a540d3bf17
Edgar Pineda <jonathanpe923@gmail.com>
2026-08-01 14:33:21 -0400

    deleted all chrome extension related code

--- index.js
+++ index.js
@@ -6,42 +6,18 @@ import { firebaseConfig} from "./config.js";
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
 
-
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.querySelector("#ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
-
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    renderLeads(myLeads)
-}
-
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log(tabs)
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        renderLeads(myLeads)
-    })
-})
 
 deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear()
-    inputEl.value = ""
-    myLeads.length = 0
-    renderLeads(myLeads)
+   
 } )
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    console.log(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads(myLeads)
 })
 
 
